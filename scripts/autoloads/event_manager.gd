82cac26a49e2a9c235f916b1232e2a5d9e426a2a
IsaelGabriel <isaelgsfaria@gmail.com>
2026-05-30 06:27:49 -0300

    Add EventManager's event management functions

--- scripts/autoloads/event_manager.gd
+++ scripts/autoloads/event_manager.gd
@@ -1,13 +1,13 @@
 ## Manages the in-game events that have to be achieved by the player.
 extends Node
 
-## List of all possible events to be checked.
+## List of all possible events to be checked (ordered).
 const EVENT_LIST: Array[StringName] = [
 	"oven_off"
 ]
 
 ## States of all current events.
-var event_states: Dictionary[StringName, bool] = {}
+var _event_states: Dictionary[StringName, bool] = {}
 
 func _ready() -> void:
 	reset()
@@ -15,4 +15,14 @@ func _ready() -> void:
 ## Resets all values of [member event_states] to [code]false[/code].
 func reset() -> void:
 	for event in EVENT_LIST:
-		event_states[event] = false
+		_event_states[event] = false
+
+## Returns the current state for [param event].[br]
+## Returns false in case the event doesn't exist.
+func get_event_state(event: StringName) -> bool: return _event_states.get(event, false)
+
+## Sets [param event] state as [code]true[/code] if it is in the list of events.
+func trigger_event(event: StringName):
+	if not _event_states.has(event): return
+	
+	_event_states[event] = true
