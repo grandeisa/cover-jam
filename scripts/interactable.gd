b39ab2a8669f83e5a911b6f61a81805edfc0bc24
IsaelGabriel <isaelgsfaria@gmail.com>
2026-05-30 03:30:03 -0300

    Add interactable object detection

---
+++ scripts/interactable.gd
@@ -0,0 +1,3 @@
+class_name Interactable extends Node
+
+signal interact
---
+++ scripts/player/interaction_controller.gd
@@ -0,0 +1,22 @@
+## Handles interaction input and detection for a player character
+class_name InteractionController extends Node
+
+## [RayCast3D] used to detect interactable objects.
+@export var _detector_ray: RayCast3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_detect_interactable()
+	
+	
+func _detect_interactable() -> void:
+	var detected_object: Node3D = _detector_ray.get_collider()
+	
+	if not detected_object: return
+	
+	print("Detected object")
