23c9ca88bf2813c16a571a9f1b5c63ddca1b984f
IsaelGabriel <isaelgsfaria@gmail.com>
2026-05-30 05:58:33 -0300

    Add separate player scene

--- scripts/player/interaction_controller.gd
+++ scripts/player/interaction_controller.gd
@@ -13,10 +13,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	_detect_interactable()
 	
-	
+
+## Check if there is an interactable object in [member _detector_ray]'s path
+## and handles interaction input.
 func _detect_interactable() -> void:
 	var detected_object = _detector_ray.get_collider()
 	
 	if detected_object is not Interactable: return
 	
-	print("Detected object")
+	# **PLACE CODE TO DISPLAY VISUAL INDICATOR HERE**
+	
+	if Input.is_action_just_pressed("p_interact"):
+		detected_object.interact.emit()
