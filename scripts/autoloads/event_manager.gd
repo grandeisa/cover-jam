4fbad653f6a5a200728bc4808ffff51ec7b3ef9d
IsaelGabriel <isaelgsfaria@gmail.com>
2026-05-30 21:30:19 -0300

    Add base for event texts on ending

--- scripts/autoloads/event_manager.gd
+++ scripts/autoloads/event_manager.gd
@@ -6,8 +6,17 @@ const EVENT_LIST: Array[StringName] = [
 	"oven_off"
 ]
 
+## Key: Events that have endings[br]
+## Value: Text displayed in case of ending (event is [code]false[/code])[br]
+## [b]All texts should be displayed with a BBCode enabled [RichTextLabel][/b]
+const _ending_texts: Dictionary[StringName, String] = {
+	"oven_off" : "Você deixou o fogão ligado. O mundo pegou fogo."
+}
+
 ## States of all current events.
 var _event_states: Dictionary[StringName, bool] = {}
+
+
 
 func _ready() -> void:
 	reset()
@@ -20,6 +29,10 @@ func reset() -> void:
 ## Returns the current state for [param event].[br]
 ## Returns false in case the event doesn't exist.
 func get_event_state(event: StringName) -> bool: return _event_states.get(event, false)
+
+## Returns the ending text for the desired event.[br]
+## Returns an empty [string] in case the ending text doesn't exist.
+func get_event_ending_text(event: StringName) -> bool: return _ending_texts.get(event, "")
 
 ## Sets [param event] state as [code]true[/code] if it is in the list of events.
 func trigger_event(event: StringName):
