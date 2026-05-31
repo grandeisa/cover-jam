e66bb279c341f18df0c169069a34b83a34d17002
IsaelGabriel <isaelgsfaria@gmail.com>
2026-05-30 22:14:52 -0300

    Add interaction hint label system

--- scripts/player/interaction_controller.gd
+++ scripts/player/interaction_controller.gd
@@ -4,13 +4,11 @@ class_name InteractionController extends Node
 ## [RayCast3D] used to detect interactable objects.
 @export var _detector_ray: RayCast3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+## Used to display the text of the interactions hint.
+@export var _interaction_hint_label: RichTextLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	_detect_interactable()
 	
 
@@ -18,10 +16,15 @@ func _process(delta: float) -> void:
 ## and handles interaction input.
 func _detect_interactable() -> void:
 	var detected_object = _detector_ray.get_collider()
-	
-	if detected_object is not Interactable: return
-	
-	# **PLACE CODE TO DISPLAY VISUAL INDICATOR HERE**
-	
-	if Input.is_action_just_pressed("p_interact"):
-		detected_object.interact.emit()
+	if detected_object is Interactable:
+		if not detected_object.active:
+			_interaction_hint_label.visible = false
+			return
+		
+		_interaction_hint_label.text = detected_object.interaction_hint
+		_interaction_hint_label.visible = true
+		
+		if Input.is_action_just_pressed("p_interact"):
+			detected_object.trigger_interaction()
+	else:
+		_interaction_hint_label.visible = false
